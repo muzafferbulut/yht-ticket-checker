18ec9cc4f5c40c8946ccfbf8be33c6bf08718a46
Muzaffer Bulut <h-muzafferb@intra.kgm.gov.tr>
2024-11-05 15:24:17 +0300

    ilgili saat aralığına göre kontrol özelliği eklendi

--- TicketChecker.py
+++ TicketChecker.py
@@ -6,7 +6,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from datetime import datetime
 from PyQt5.uic import loadUi
+import pandas as pd
 import time
 import sys
 import json
@@ -35,10 +37,13 @@ class TicketChecker(QMainWindow):
         self.worker.start()
 
     def checkControl(self, controlMessage):
-        pass
+        now = datetime.now()
+        now = now.strftime("%H:%M:%S")
+        message = f"Kontrol zamanı : {now}, {controlMessage}"
+        self.logPlainTextEdit.appendPlainText(message)
 
     def onControlFinished(self):
-        pass
+        QMessageBox.information(self, "Kontrol Tamamlandı", "Bilet kontrolü tamamlandı!")
 
     def addChromeDriver(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Exe Dosyası Seç", "", "Exe Files (*.exe)")
@@ -69,6 +74,7 @@ class Controller(QThread):
     controlMessage = pyqtSignal(str)
 
     def __init__(self, dependencies, departureStation, arrivalStation, travelDate, travelTime, controlTime):
+        super().__init__()
         self.dependencies = dependencies
         self.departureStation = departureStation
         self.arrivalStation = arrivalStation
@@ -77,63 +83,73 @@ class Controller(QThread):
         self.controlTime = controlTime
 
     def run(self):
-        service = Service(self.dependencies["driver_path"])
-        options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome(service=service, options=options)
+        try:
+            service = Service(self.dependencies["driver_path"])
+            options = webdriver.ChromeOptions()
+            # options.add_argument("--headless")
+            driver = webdriver.Chrome(service=service, options=options)
 
-        driver.get(self.dependencies["tcdd_link"])
-        time.sleep(3)
+            driver.get(self.dependencies["tcdd_link"])
+            time.sleep(3)
 
-        wait = WebDriverWait(driver, 10)
+            wait = WebDriverWait(driver, 10)
+            
+            # departure
+            departure = wait.until(EC.presence_of_element_located((By.ID, self.dependencies["departure_element_id"])))
+            departure.clear()
+            departure.send_keys(self.departureStation)
+            time.sleep(3)
+            departure.send_keys(Keys.ENTER)
+
+            # arrival
+            arrival = driver.find_element(By.ID, self.dependencies["arrival_element_id"])
+            arrival.clear()
+            arrival.send_keys(self.arrivalStation)
+            time.sleep(3)
+            arrival.send_keys(Keys.ENTER)
+            
+            time.sleep(3)
+
+            # date input
+            dateInput = driver.find_element(By.ID, self.dependencies["departure_date_element_id"])
+            dateInput.clear()
+            dateInput.send_keys(self.date)
+            dateInput.send_keys(Keys.ENTER)
+
+            time.sleep(3)
+
+            # close date tab
+            close_date_tab = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, self.dependencies["departure_date_element_close_button_css_selector"]))
+            )    
+            close_date_tab.click()
+
+            searchButton = driver.find_element(By.ID, self.dependencies["search_button_id"])
+            searchButton.click()
+            time.sleep(10)
+
+            rows = driver.find_elements(By.CSS_SELECTOR, self.dependencies["ticket_control_css_selector"])
+            data = []
+
+            for row in rows:
+                cells = row.find_elements(By.TAG_NAME, "td")
+                row_data = [cell.text for cell in cells]
+                data.append(row_data)
+
+            df = pd.DataFrame(data)
+            filtered_df = df[df[0].str.contains(self.time)].values
+
+            self.controlMessage.emit(filtered_df[0][4])
+
+        except Exception as e:
+            self.controlMessage.emit(f"Hata: {e}")
         
-        # departure
-        departure = wait.until(EC.presence_of_element_located((By.ID, self.dependencies["departure_element_id"])))
-        departure.clear()
-        departure.send_keys(self.departureStation)
-        time.sleep(3)
-        departure.send_keys(Keys.ENTER)
-
-        # arrival
-        arrival = driver.find_element(By.ID, self.dependencies["arrival_element_id"])
-        arrival.clear()
-        arrival.send_keys(self.arrivalStation)
-        time.sleep(3)
-        arrival.send_keys(Keys.ENTER)
-        
-        time.sleep(3)
-
-        dateInput = driver.find_element(By.ID, self.dependencies["departure_date_element_id"])
-        dateInput.clear()
-        dateInput.send_keys(self.date)
-        dateInput.send_keys(Keys.ENTER)
-
-        time.sleep(3)
-
-        close_date_tab = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, self.dependencies["departure_date_elemen_close_button_css_selector"]))
-        )    
-        close_date_tab.click()
-
-        searchButton = driver.find_element(By.ID, self.dependencies["search_button_id"])
-        searchButton.click()
-        time.sleep(10)
-
-        bilet_durumu = driver.find_elements(By.CSS_SELECTOR, self.dependencies["ticket_control_css_selector"])
-
-        etiketler = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, self.dependencies["ticket_control_row_label_class"]))
-        )
-
-        bilet_list = []
-        for etiket in etiketler:
-            bilet_list.append(etiket.text)
-            print(etiket.text)
-
-        print(bilet_list[-7])
-        print(bilet_list[-8])
+        finally:
+            driver.quit()
+            self.finished.emit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    report = TicketChecker()
-    report.show()
+    checker = TicketChecker()
+    checker.show()
     sys.exit(app.exec_())
